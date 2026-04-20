c58d7e78969d090c6a446e66ab20e8dc571ab886
Danial Oberg <1007014+Incredible0n3@users.noreply.github.com>
2026-04-19 21:25:42 -0700

    monsters[:feat]: creates initial monsters
    adds monsters  and collision system
    
    closes: 2
    Signed-off-by: Danial Oberg <1007014+Incredible0n3@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,8 @@ mod prelude {
     pub use legion::*;
     pub use legion::world::SubWorld;
     pub use legion::systems::CommandBuffer;
-    pub const SCREEN_WIDTH: i32 = 80;
-    pub const SCREEN_HEIGHT: i32 = 50;
+    pub const SCREEN_WIDTH: i32 = 60;
+    pub const SCREEN_HEIGHT: i32 = 60;
     pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
     pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
     pub use crate::components::*;
@@ -39,6 +39,11 @@ impl State {
         let mut rng = RandomNumberGenerator::new();
         let map_builder = MapBuilder::new(&mut rng);
         spawn_player(&mut ecs, map_builder.player_start);
+        map_builder.rooms
+            .iter()
+            .skip(1)
+            .map(|r| r.center())
+            .for_each(|pos| spawn_monster(&mut ecs, &mut rng, pos));
         resources.insert(map_builder.map);
         resources.insert(Camera::new(map_builder.player_start));
         Self {
