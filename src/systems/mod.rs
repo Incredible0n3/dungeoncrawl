a27bd39257dafd5958c40d0dc9d9e837ba7baea4
Danial Oberg <1007014+Incredible0n3@users.noreply.github.com>
2026-04-24 23:06:26 -0700

    health
    Signed-off-by: Danial Oberg <1007014+Incredible0n3@users.noreply.github.com>

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -3,15 +3,16 @@ use crate::prelude::*;
 mod map_render;
 mod entity_render;
 mod player_input;
-mod collisions;
 mod random_move;
 mod end_turn;
 mod movement;
+mod hud;
+mod tooltips;
+mod combat;
 
 pub fn build_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(player_input::player_input_system())
-        .add_system(collisions::collisions_system())
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
@@ -25,17 +26,20 @@ pub fn build_input_scheduler() -> Schedule {
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
+        .add_system(hud::hud_system())
+        .add_system(tooltips::tooltips_system())
         .build()
 }
 
 pub fn build_player_scheduler() -> Schedule {
     Schedule::builder()
-        .add_system(movement::movement_system())
+        .add_system(combat::combat_system())
         .flush()
-        .add_system(collisions::collisions_system())
+        .add_system(movement::movement_system())
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
+        .add_system(hud::hud_system())
         .add_system(end_turn::end_turn_system())
         .build()
 }
@@ -44,10 +48,13 @@ pub fn build_monster_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(random_move::random_move_system())
         .flush()
+        .add_system(combat::combat_system())
+        .flush()
         .add_system(movement::movement_system())
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
+        .add_system(hud::hud_system())
         .add_system(end_turn::end_turn_system())
         .build()
 }
