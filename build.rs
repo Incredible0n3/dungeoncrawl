0753ddc3e45df487003929a3be97878ddc771867
Danial Oberg <1007014+Incredible0n3@users.noreply.github.com>
2026-04-27 11:39:03 -0700

    fov[:feat]: allows player to only see in fov
    adds field of view map restriction
    
    closes: #9
    Signed-off-by: Danial Oberg <1007014+Incredible0n3@users.noreply.github.com>

--- build.rs
+++ build.rs
@@ -6,7 +6,21 @@ fn main() {
     let mut res = winres::WindowsResource::new();
     res.set_icon_with_id("resources/knight.ico", "1")
         .set("InternalName", "DUNGEONCRAWL.EXE")
+        .set("RegisteredOrganization", "Rip City Technologies")
         .set_version_info(winres::VersionInfo::PRODUCTVERSION, 0x0001000000000000);
+    res.set("CompanyName", "Rip City Technologies"); 
+    res.set("ProductName", "Dungeon Crawl");
+    res.set_manifest(r#"
+    <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+        <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+            <security>
+                <requestedPrivileges>
+                    <requestedExecutionLevel level="asInvoker" uiAccess="true"/>
+                </requestedPrivileges>
+            </security>
+        </trustInfo>
+    </assembly>
+    "#);
     res.compile().unwrap();
 }
 
