ea5996c8e52f58b3b7bc15ffefc719d39aef6051
Danial Oberg <1007014+Incredible0n3@users.noreply.github.com>
2026-05-03 10:37:38 -0700

    levels[:feat]: adds levels
    adds game levels, keeps inventory and changes the monsters, map
    
    closes: #14
    Signed-off-by: Danial Oberg <1007014+Incredible0n3@users.noreply.github.com>

--- src/systems/end_turn.rs
+++ src/systems/end_turn.rs
@@ -5,8 +5,10 @@ use crate::prelude::*;
 #[read_component(Point)]
 #[read_component(Player)]
 #[read_component(AmuletOfYala)]
-pub fn end_turn(ecs: &SubWorld, 
-                #[resource] turn_state: &mut TurnState
+pub fn end_turn(
+    ecs: &SubWorld, 
+    #[resource] turn_state: &mut TurnState,
+    #[resource] map: &Map
 ) {
     let mut player_hp = <(&Health, &Point)>::query()
         .filter(component::<Player>());
@@ -20,10 +22,11 @@ pub fn end_turn(ecs: &SubWorld,
         _ => current_state
     };
 
+    let amulet_default = Point::new(-1,-1);
     let amulet_pos = amulet
         .iter(ecs)
         .nth(0)
-        .unwrap();
+        .unwrap_or(&amulet_default);
 
     player_hp.iter(ecs).for_each(|(hp, pos)| {
         if hp.current < 1 {
@@ -31,6 +34,10 @@ pub fn end_turn(ecs: &SubWorld,
         }
         if pos == amulet_pos {
             new_state = TurnState::Victory;
+        }
+        let idx = map.point2d_to_index(*pos);
+        if map.tiles[idx] == TileType::Exit {
+            new_state = TurnState::NextLevel;
         }
     });
     *turn_state = new_state;
